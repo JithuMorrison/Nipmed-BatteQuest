e5f54a591f8df1945259639181fd0bdf7d3d9c81
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2025-01-15 13:53:08 +0530

    Create BodySourceView.cs

---
+++ BodySourceView.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+using Windows.Kinect;
+using Joint = Windows.Kinect.Joint;
+
+public class BodySourceView : MonoBehaviour
+{
+    public BodySourceManager mBodySourceManager;
+    public GameObject mJointObject;
+    public GameObject mFaceObject; // New GameObject for the face
+
+    private Dictionary<ulong, GameObject> mBodies = new Dictionary<ulong, GameObject>();
+    private List<JointType> _joints = new List<JointType>
+    {
+        JointType.HandLeft,
+        JointType.HandRight,
+    };
+
+    private List<JointType> _facialFeatures = new List<JointType>
+    {
+        JointType.Head, // Use JointType if facial features are supported
+        // Add other facial features if available
+    };
+
+    void Update()
+    {
+        #region Get Kinect data
+        Body[] data = mBodySourceManager.GetData();
+        if (data == null)
+            return;
+
+        List<ulong> trackedIds = new List<ulong>();
+        foreach (var body in data)
+        {
+            if (body == null)
+                continue;
+
+            if (body.IsTracked)
+                trackedIds.Add(body.TrackingId);
+        }
+        #endregion
+
+        #region Delete Kinect bodies
+        List<ulong> knownIds = new List<ulong>(mBodies.Keys);
+        foreach (ulong trackingId in knownIds)
+        {
+            if (!trackedIds.Contains(trackingId))
+            {
+                // Destroy body object
+                Destroy(mBodies[trackingId]);
+
+                // Remove from list
+                mBodies.Remove(trackingId);
+            }
+        }
+        #endregion
+
+        #region Create Kinect bodies
+        foreach (var body in data)
+        {
+            // If no body, skip
+            if (body == null)
+                continue;
+
+            if (body.IsTracked)
+            {
+                // If body isn't tracked, create body
+                if (!mBodies.ContainsKey(body.TrackingId))
+                    mBodies[body.TrackingId] = CreateBodyObject(body.TrackingId);
+
+                // Update positions
+                UpdateBodyObject(body, mBodies[body.TrackingId]);
+                UpdateFaceObject(body, mBodies[body.TrackingId]);//Face
+            }
+        }
+        #endregion
+    }
+
+    private GameObject CreateBodyObject(ulong id)
+    {
+        // Create body parent
+        GameObject body = new GameObject("Body:" + id);
+
+        // Create joints
+        foreach (JointType joint in _joints)
+        {
+            // Create Object
+            GameObject newJoint = Instantiate(mJointObject);
+            newJoint.name = joint.ToString();
+
+            // Parent to body
+            newJoint.transform.parent = body.transform;
+        }
+
+        // Create face object if needed
+        GameObject face = Instantiate(mFaceObject);
+        face.name = "Face";
+        face.transform.parent = body.transform;
+
+        return body;
+    }
+
+    private void UpdateBodyObject(Body body, GameObject bodyObject)
+    {
+        // Update joints
+        foreach (JointType _joint in _joints)
+        {
+            // Get new target position
+            Joint sourceJoint = body.Joints[_joint];
+            Vector3 targetPosition = GetVector3FromJoint(sourceJoint);
+            targetPosition.z = 0;
+
+            // Get joint, set new position
+            Transform jointObject = bodyObject.transform.Find(_joint.ToString());
+            jointObject.position = targetPosition;
+        }
+    }
+
+    private void UpdateFaceObject(Body body, GameObject bodyObject)
+    {
+        // Assuming facial features are available in the body.Joints
+        foreach (JointType _facialFeature in _facialFeatures)
+        {
+            Joint sourceJoint = body.Joints[_facialFeature];
+            Vector3 targetPosition = GetVector3FromJoint(sourceJoint);
+            targetPosition.z = 0;
+
+            Transform faceObject = bodyObject.transform.Find("Face");
+            // Update position of the face object if needed
+            faceObject.position = targetPosition;
+        }
+    }
+
+    private Vector3 GetVector3FromJoint(Joint joint)
+    {
+        return new Vector3(joint.Position.X * 10, joint.Position.Y * 10, joint.Position.Z * 10);
+    }
+}
